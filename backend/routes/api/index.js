5cc3c433c313e611e42ef18dc5f2723dc8ad66e1
dasch22 <michaeldasch22@gmail.com>
2022-06-24 13:38:26 -0700

    user auth set-up

---
+++ backend/routes/api/index.js
@@ -0,0 +1,12 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+module.exports = router;
+
+//mq6Ivigp-J118u00TUdzHbihEAO7bxUFYzPA
