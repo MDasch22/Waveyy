917852894376650f3754f195e3753114a133af18
dasch22 <michaeldasch22@gmail.com>
2022-07-03 16:54:31 -0700

    seeded more beaches and reviews

--- backend/db/seeders/20220623204705-demo-user.js
+++ backend/db/seeders/20220623204705-demo-user.js
@@ -7,19 +7,19 @@ module.exports = {
       {
         profilePic: "https://www.ledgerinsights.com/wp-content/uploads/2021/12/adidas-nft-bored-ape.jpg",
         email: 'demo@user.io',
-        username: 'Guest',
+        username: 'Demo',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         profilePic: "https://www.wavysurfcamp.com/wp-content/uploads/2018/04/clarklittle00-1.jpg",
         email: 'user1@user.io',
-        username: 'Guest2',
+        username: 'Michael Dasch',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         profilePic: 'https://i.pinimg.com/736x/5c/40/d7/5c40d70ae48e570e80fdea39c27a9a7f--surfer-girls-surfing.jpg',
         email: 'user2@user.io',
-        username: 'Guest3',
+        username: 'Steve Harrington',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
